a426fb63557589960a62ccebf35a29b5aafa8565
Schettler-Köhler <jan-gerrit.schettler-koehler@capgemini.com>
2020-03-22 18:22:41 +0100

    Fehlerbehandlung

--- client/src/main/web/src/app/app.reducer.ts
+++ client/src/main/web/src/app/app.reducer.ts
@@ -1,17 +1,25 @@
 import { createReducer, on } from '@ngrx/store';
 import { initialState } from './app.state';
-import { createOrderSuccess } from './order/order.actions';
+import { createOrderSuccess, createOrderError, getProductsForStoreError } from './order/order.actions';
+import { getOrdersError, bringOrderError } from './store-orders/store-orders.actions';
+import { searchForPlzError } from './select-store/select-store.actions';
 
+const genericErrorMessage = 'Es ist ein Fehler aufgetreten. Bitte versuchen Sie es erneut.';
+function setGenericErrorMessage(state) {
+    return { ...state,
+        uiState: {
+            ...state.uiState,
+            message: genericErrorMessage
+        }
+    };
+}
 
 const appReducerCreator = createReducer(initialState,
-    on(createOrderSuccess, (state, payload) => {
-        return { ...state,
-            uiState: {
-                ...state.uiState,
-                message: 'Ihre Bestellung wurde angelegt.'
-            }
-        };
-    })
+    on(createOrderError, setGenericErrorMessage),
+    on(getOrdersError, setGenericErrorMessage),
+    on(getProductsForStoreError, setGenericErrorMessage),
+    on(searchForPlzError, setGenericErrorMessage),
+    on(bringOrderError, setGenericErrorMessage)
 );
 
 export function appReducer(state, action) {
